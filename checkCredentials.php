dc25dc129ad7605fa7e8fa26528494089ad2085c
Shivam Prajapati <shivamsprajapati13@gmail.com>
2021-09-27 16:06:53 +0530

    check Credentials changes

--- checkCredentials.php
+++ checkCredentials.php
@@ -6,8 +6,8 @@ require('connection.php');
 	$sql = "select * from users where `username`='$username' and `password`='$password'";
 	$res = mysqli_query($conn,$sql);
 	$count = mysqli_num_rows($res);
-    echo $sql;
-    echo $count;
+    // echo $sql;
+    // echo $count;
   if ($count > 0)
   {
     $_SESSION['LOGIN']='yes';
@@ -15,8 +15,16 @@ require('connection.php');
     header('location:dash.php'); 
  }
  else{
-     header('location:login.php');
- }
+     ?>
+     <script type = "text/javascript"> 
+     alert("Invalid Username or Password");
+     window.location.href = "login.php";
+     </script>
+
+     <?php
+
+//  header('location:login.php'); 
+}
   
 
 ?>
